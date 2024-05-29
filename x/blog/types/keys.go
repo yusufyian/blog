42c87e19886244d9b2ce62cc2e90c536f02a20a9
yusufyian <yussufer@gmail.com>
2024-05-29 07:05:40 +0000

    chore: Remove unused imports and add post-related keys

--- x/blog/types/keys.go
+++ x/blog/types/keys.go
@@ -9,6 +9,13 @@ const (
 
 	// MemStoreKey defines the in-memory store key
 	MemStoreKey = "mem_blog"
+
+	// PostKey is used to uniquely identify posts within the system.
+	// It will be used as the beginning of the key for each post, followed bei their unique ID
+	PostKey = "Post/value/"
+
+	// This key will be used to keep track of the ID of the latest post added to the store.
+	PostCountKey = "Post/count/"
 )
 
 var (
